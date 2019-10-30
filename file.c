fa953de4cb5b1489cd79990c208b03712af7c022
Priya2597 <57185541+Priya2597@users.noreply.github.com>
2019-10-30 20:45:16 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int,int);
+int multi(int,int);
+
+void main()
+{
+    int a=10, b=20;
+    int c;
+    int d;
+    printf("Enter c:");
+    scanf("%d",&c);
+
+    int (*fptr)(int,int);
+    if(c==1)
+        fptr=sum;
+    else
+        fptr=multi;
+    d=fptr(a,b);
+    printf("%d",d);
+}
+
+int sum(int x,int y)
+{
+    return x+y;
+
+}
+int multi(int x,int y)
+{
+    return x*y;
+
+}
+
